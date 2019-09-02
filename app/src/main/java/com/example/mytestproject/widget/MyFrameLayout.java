80e65e472c796bce0171cbd5702809ebd428ca1c
xingzhongzhang <xingzhongzhang@sohu-inc.com>
2019-09-02 17:07:02 +0800

    添加computeScroll

--- app/src/main/java/com/example/mytestproject/widget/MyFrameLayout.java
+++ app/src/main/java/com/example/mytestproject/widget/MyFrameLayout.java
@@ -2,26 +2,33 @@ package com.example.mytestproject.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
+import android.widget.Scroller;
 
 public class MyFrameLayout extends FrameLayout {
+    private static final String TAG = "MyFrameLayout";
     public MyFrameLayout(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public MyFrameLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public MyFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        scroller = new Scroller(context);
     }
+    private Scroller scroller;
 
     private int motionY;
     private int lastY;
     private boolean isDragging = false;
+
+    private boolean up = false;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -37,6 +44,7 @@ public class MyFrameLayout extends FrameLayout {
                 int distY = y - motionY;
                 if (!isDragging && Math.abs(distY) > ViewConfiguration.get(getContext()).getScaledTouchSlop()) {
                     isDragging = true;
+                    up = distY < 0;
                 }
 
                 if (isDragging) {
@@ -48,6 +56,12 @@ public class MyFrameLayout extends FrameLayout {
             }
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP: {
+                if (isDragging) {
+                    Log.e(TAG, "onTouchEvent() scrollY = " + getScrollY());
+                    scroller.startScroll(getScrollX(), getScrollY() - 1,  0, up ? 100 : -100, 1000);
+                    postInvalidate();
+                }
+                up = false;
                 isDragging = false;
                 motionY = 0;
                 lastY = 0;
@@ -55,5 +69,15 @@ public class MyFrameLayout extends FrameLayout {
             }
         }
         return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (!isDragging && scroller.computeScrollOffset()) {
+            Log.e(TAG, "computeScroll() currY = " + scroller.getCurrY());
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
+        }
+        Log.e(TAG, "computeScroll() scrollY = " + getScrollY());
     }
 }
